e6edfe9b9789bb8778dfc2f40084ad66f167b79e
Tianfeng Du <admiralight@gmail.com>
2021-01-06 01:01:38 -0500

    0.0.5

--- src/components/SatelliteList.js
+++ src/components/SatelliteList.js
@@ -1,27 +1,50 @@
 import React, {Component} from 'react';
-import { List, Avatar, Button, Checkbox, Spin } from 'antd';
+import { List, Avatar, Button, Checkbox, Spin, InputNumber } from 'antd';
 import Satellite from "../assets/images/Satellite.svg";
 
 class SatelliteList extends Component {
+    constructor(){
+        super();
+        this.state = {
+            duration: 0,
+        }
+    }
+
+
     onChange = e => {
         const { dataInfo, checked } = e.target;
         this.props.onSelectionChange(dataInfo, checked);
     }
 
+    onChangeDuration = (value) => {
+        this.setState({
+            duration: value
+        })
+    }
 
     render() {
         const satList = this.props.satInfo ? this.props.satInfo.above : [];
-
+        
         return (
             <div className="sat-list-box">
                 <Button 
                     className="sat-list-btn"
                     size="large"
                     disabled={this.props.disableTrack}
-                    onClick={() => this.props.trackOnclick()}
+                    onClick={() => this.props.trackOnclick(this.state.duration)}
                 >
-                    Track on the map
+                            Track on the map
                 </Button>
+                <div className="list-item">
+                    <label>Track Duration </label>
+                    <InputNumber
+                        min={0}
+                        max={90}
+                        defaultValue={0}
+                        style={{margin: "0 2px"}}
+                        onChange={this.onChangeDuration}
+                    />
+                </div>
                 <hr/>
                 {
                     this.props.loading ?
